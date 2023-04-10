cc78171e0798de0b92deea5a7428c358e5c921e2
Mohamed Abotalb <mohamedabotalb276@gmail.com>
2023-04-10 03:34:59 +0200

    Complete Task 8

---
+++ 1. Class and Objects/Task 8/Student.ts
@@ -0,0 +1,19 @@
+/*
+ * Task 8:
+ * Write a program to declare a Student class of the following information:
+ * name and age are attributes with private modifier.
+ * Student() is a no-arg constructor with public modifier.
+ * getInformation() and display() are methods with public modifier.
+ */
+
+class Student {
+  private name: string = "";
+  private age: number = 0;
+
+  // setting the Student() constructor
+  public constructor() {}
+
+  public getInformation(): void {}
+
+  public display(): void {}
+}
