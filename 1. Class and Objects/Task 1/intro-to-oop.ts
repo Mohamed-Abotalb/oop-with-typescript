3028d811e15a31b974cf192807fc91274c374154
Mohamed Abotalb <mohamedabotalb276@gmail.com>
2023-04-10 03:09:36 +0200

    Complete Task 1

---
+++ 1. Class and Objects/Task 1/intro-to-oop.ts
@@ -0,0 +1,7 @@
+/*
+ * Task 1:
+ * What is object-oriented programming?
+ *      Object-oriented programming is simply understood as a paradigm to solve
+ *      programming problems that are easier to develop and maintain when applying code,
+ *      this is an advanced paradigm that is suitable for developing most applications today
+ */
