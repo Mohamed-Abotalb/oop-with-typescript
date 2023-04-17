172e844eaa9267642edc8484d94d353bd55ad455
Mohamed Abotalb <mohamedabotalb276@gmail.com>
2023-04-17 18:06:29 +0200

    Complete Task 30

---
+++ 4. Inheritance/Task 30/Multiple-Inheritance.ts
@@ -0,0 +1,12 @@
+/*
+ * Task 30:
+ * Does TypeScript support multiple inheritance? No.
+ *
+ * Multiple inheritance means a class can inherit characteristics and features from more than one parent class.
+ *
+ * The problem with multiple inheritance:
+ *
+ * Assuming that you have 3 classes A, B and C where A and B share the same method which is show() method,
+ * class C inherits characteristics of the above two methods. So whether class A or B was the show() method
+ * in class C inherited from?
+ */
