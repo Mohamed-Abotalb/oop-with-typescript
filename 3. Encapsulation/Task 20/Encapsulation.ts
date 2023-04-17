930d3522a71d2ac036f86e18ceef3ab26b230735
Mohamed Abotalb <mohamedabotalb276@gmail.com>
2023-04-17 02:39:40 +0200

    Add section 3 and complete Task 20

---
+++ 3. Encapsulation/Task 20/Encapsulation.ts
@@ -0,0 +1,18 @@
+/*
+ * Task 20:
+ * What are benefits of encapsulation in object oriented programming?
+ *
+ * Encapsulation is a mechanism of wrapping the data (variables) and code acting on the
+ * data (methods) together as a single unit.
+ * In encapsulation the variables of a class will be hidden from other classes and can
+ * be accessed only through the methods of their current class.
+ * We should declare the variables of a class as private to achieve encapsulation and provide
+ * public setter and getter methods to modify and view the values the variables values.
+ *
+ * The benefits of Encapsulation:
+ *      The fields of a class can be made read-only or write-only.
+ *      A class can have total control over what is stored in its fields.
+ *      Hiding unnecessary information of the object.
+ *      Allowing you to change the structure inside class without affecting the other classes
+ *
+ */
